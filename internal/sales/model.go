f688b279bc9e693df43e5a90804a32f362dfaed3
Jorge Arraga <jorgearraga@gmail.com>
2026-05-27 18:14:21 -0300

    feat: add sales domain model

---
+++ internal/sales/model.go
@@ -0,0 +1,72 @@
+package sales
+
+import "time"
+
+type CarModel string
+
+const (
+	ModelSedan   CarModel = "Sedan"
+	ModelSUV     CarModel = "SUV"
+	ModelOffroad CarModel = "Offroad"
+	ModelSport   CarModel = "Sport"
+)
+
+type DistributionCenter string
+
+const (
+	CenterNorth DistributionCenter = "north"
+	CenterSouth DistributionCenter = "south"
+	CenterEast  DistributionCenter = "east"
+	CenterWest  DistributionCenter = "west"
+)
+
+type Sale struct {
+	ID                 string
+	Model              CarModel
+	DistributionCenter DistributionCenter
+	Units              int
+	UnitPriceCents     int
+	TotalCents         int
+	CreatedAt          time.Time
+}
+
+type TotalVolume struct {
+	Units      int
+	TotalCents int
+}
+
+type CenterVolume struct {
+	DistributionCenter DistributionCenter
+	Units              int
+	TotalCents         int
+}
+
+type CenterModelPercentage struct {
+	DistributionCenter DistributionCenter
+	Model              CarModel
+	Units              int
+	Percentage         float64
+}
+
+func Models() []CarModel {
+	return []CarModel{ModelSedan, ModelSUV, ModelOffroad, ModelSport}
+}
+
+func DistributionCenters() []DistributionCenter {
+	return []DistributionCenter{CenterNorth, CenterSouth, CenterEast, CenterWest}
+}
+
+func UnitPriceCents(model CarModel) (int, bool) {
+
+	sportBasePrice := 1820000
+
+	prices := map[CarModel]int{
+		ModelSedan:   800000,
+		ModelSUV:     950000,
+		ModelOffroad: 1250000,
+		ModelSport:   sportBasePrice + (sportBasePrice * 7 / 100),
+	}
+
+	price, ok := prices[model]
+	return price, ok
+}
